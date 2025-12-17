40da238dbee56c587f9375fc4ad9e174f7971b94
instrutorvilson <instrutorvilsonmoro@gmail.com>
2025-12-17 19:39:12 -0300

    Gravando no banco mysql

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,14 +2,48 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const db = require('./conectaDB')
+
 const { validaProduto } = require('./middlewares/middleware')
 
 app.use(express.json())
 
-app.post('/api/v1/produto', validaProduto, (req, res) => {
-   //gravar no banco
-   res.status(201).send(req.body);
+app.post('/api/v1/produto', validaProduto, async (req, res) => {
+   const { nome, preco } = req.body
+   const result = await db.execute('insert into produto (nome, preco)values(?,?)',[nome, preco])
+   res.status(201).send({ mensagem:'Produto salvo com sucesso', content: {id:result.insertId, nome, preco}});
 });
+
+app.get('/api/v1/produto', async (req, res) => {
+   const rows = await db.execute('select * from produto')
+   res.status(200).json(rows);
+});
+
+
+
+/**app.post('/api/v1/produto', (req, res) => { 
+    const novoProduto = {
+        nome:req.body.nome,
+        preco:req.body.preco,
+        id:0
+    }   
+    con.connect(function(err) {
+        if (err) {
+            res.status(501).send({ mensagem: 'Erro ao salvar produto', content: err});
+            return
+        }
+        let sql = `INSERT INTO produto (nome, preco) VALUES ('${req.body.nome}', '${req.body.preco}')`;
+        con.query(sql, function (err, result) {
+            if (err) {
+               res.status(501).send({ mensagem: 'Erro ao salvar produto', content: err});
+              return
+            } 
+            novoProduto.id = result.insertId   
+            res.status(201).send({ mensagem: 'Produto salvo com sucesso', content: novoProduto});        
+        });
+    });     
+});
+ */
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
