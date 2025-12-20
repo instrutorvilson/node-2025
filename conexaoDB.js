2693c975087c6d56d9ea1775f28ae1482e790af6
instrutorvilsonmoro <instrutorvilsonmoro@gmail.com>
2025-12-20 12:21:15 -0300

    configurar variaveis de ambiente

--- conexaoDB.js
+++ conexaoDB.js
@@ -1,9 +1,13 @@
 const mysql = require('mysql2')
 
 module.exports = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "aula_node"
+    host: process.env.HOST,
+    user: process.env.USER_NAME,
+    password: process.env.SENHA,
+    database: process.env.DATABASE
+    /*host:'localhost',
+    user:'root',
+    password:'admin',
+    database:'aula_node'*/
   });
 
